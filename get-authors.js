cb0365030d39847dc0c48980d065f039768a2931
vintagefuture <pinkpallyn@gmail.com>
2024-04-18 01:13:05 +0100

    authors working!
    Change-Id: I44e9a97250a1899806745ffe1528c89d0aaea60e

--- get-authors.js
+++ get-authors.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("authors-div");
     const authorsList = document.getElementById("authors-list");
     const paginationContainer = document.getElementById("pagination");
+    const authorsQuote = document.getElementById("authors-quote");
+    const selectedAuthorTitle = document.getElementById(
+        "selected-author-title"
+    );
+    const authorsMainDiv = document.getElementById("authors-main-div");
 
     const desiredRows = 5;
 
@@ -26,6 +31,55 @@ document.addEventListener("DOMContentLoaded", () => {
                     authorLink.href = `https://api.quotable.io/random?author=${encodeURIComponent(
                         element.name
                     )}`;
+                    authorLink.addEventListener("click", async (event) => {
+                        event.preventDefault();
+                        try {
+                            const authorResponse = await fetch(authorLink.href);
+                            const authorData = await authorResponse.json();
+                            const anotherQuoteButton =
+                                document.createElement("button");
+                            anotherQuoteButton.textContent = "Get another one";
+                            anotherQuoteButton.className = "button";
+
+                            if (authorResponse.ok) {
+                                authorsQuote.textContent = authorData.content;
+
+                                container.innerHTML = "";
+                                paginationContainer.innerHTML = "";
+
+                                selectedAuthorTitle.textContent = `Random quote by ${authorData.author}`;
+                                authorsMainDiv.appendChild(anotherQuoteButton);
+                                anotherQuoteButton.addEventListener(
+                                    "click",
+                                    async () => {
+                                        try {
+                                            const randomQuoteResponse =
+                                                await fetch(
+                                                    `https://api.quotable.io/random?author=${encodeURIComponent(
+                                                        authorData.author
+                                                    )}`
+                                                );
+                                            const randomQuoteData =
+                                                await randomQuoteResponse.json();
+                                            if (randomQuoteResponse.ok) {
+                                                authorsQuote.textContent = "";
+                                                authorsQuote.textContent =
+                                                    randomQuoteData.content;
+                                            } else {
+                                                console.error(randomQuoteData);
+                                            }
+                                        } catch (error) {
+                                            console.error(error);
+                                        }
+                                    }
+                                );
+                            } else {
+                                console.error(authorData);
+                            }
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    });
                     const authorListItem = document.createElement("li");
                     authorListItem.appendChild(authorLink);
                     authorsList.appendChild(authorListItem);
