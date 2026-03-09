0e41d700dd96a0159985081f4799f60bee6d63b7
nedo <pinkpallyn@gmail.com>
2026-03-09 16:48:43 +0000

    adding github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // Replace with your site URL
+  site: 'https://vintagefuture.github.io', // Replace with your site URL
   integrations: [
     tailwind(),
     sitemap(),
